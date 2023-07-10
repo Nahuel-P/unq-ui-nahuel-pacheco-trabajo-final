7cbcc32fb48f237639c3373671f7574f66406f61
Nahuel Pacheco <pachecoalannahuel@gmail.com>
2023-07-10 20:29:05 -0300

    add: 404

--- src/components/pages/NotFoundPage.jsx
+++ src/components/pages/NotFoundPage.jsx
@@ -1,8 +1,19 @@
 import React from "react";
+import spock from "./spock404.gif";
+import "./NotFoundPage.css";
 
 const NotFoundScreen = () => {
 
-  return <div>404</div>
+  return (
+    <>
+    <h1 className="not-found"> No encontramos la página que estás buscando</h1>
+    <img
+      src={spock}
+      alt="spock"
+      className="not-found-gif"
+    />
+    </>
+  );
 };
 
 export default NotFoundScreen;
