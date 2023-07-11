758f54a0d924d71be3c3a64bcc7aee99560edc78
Nahuel Pacheco <pachecoalannahuel@gmail.com>
2023-07-11 02:11:39 -0300

    add: minor

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import {Route, Routes } from 'react-router-dom';
 import MainScreen from './components/pages/MainScreen';
 import NotFoundPage from './components/pages/NotFoundPage';
 
