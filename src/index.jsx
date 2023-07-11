a56af42e40e811c3df751effd2cf7165a9ba35d2
Nahuel Pacheco <pachecoalannahuel@gmail.com>
2023-07-11 02:06:48 -0300

    fix: routes

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
 import App from "./App";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/unq-ui-nahuel-pacheco-trabajo-final">
       <App />
     </BrowserRouter>
   </React.StrictMode>
